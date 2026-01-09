ace21098d01211eaa54df048765397efe3c419c1
小二 <17801063691@139.com>
2026-01-09 14:21:35 +0800

    redis

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from crawl4ai import AsyncWebCrawler
 import asyncio
+from redis.asyncio import *
+REDIS_URL="rediss://default:ASLRAAImcDFlZDM0YzQ3NTUxNTI0MjliYmVjNGFiYjBmZTExNzA2MXAxODkxMw@balanced-hagfish-8913.upstash.io:6379"
+redis = Redis.from_url(REDIS_URL, decode_responses=True)
 
 
 async def main():
     async with AsyncWebCrawler() as crawler:
+        print(await redis.ping())
+        print(await redis.get("name"))
         res = await crawler.arun("https://www.google.com")
         print(res)
 if __name__ == '__main__':
